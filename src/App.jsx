f49a13a1f4241a94b0bf7d5d95e33bdb8ae2607f
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-07 15:23:33 -0600

    refactor: Update App component to include Google sign-in functionality
    The App component has been updated to include the Google sign-in functionality using the `signInWithGoogle` method from the `firebaseApp` module. This change allows users to sign in with their Google accounts and improves the authentication flow by redirecting them to the Google sign-in page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
-import React, { useState } from 'react'
-import { AuthProvider } from './context/auth.context'
-import firebaseApp from './config/firebase';
-import { redirect } from 'react-router-dom';
-import Home from './pages/Home/Home';
+import React, { useState } from "react";
+import { AuthProvider } from "./context/auth.context";
+import firebaseApp from "./config/firebase";
+import { redirect } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 export default function App() {
+  const [user, setUser] = useState({});
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const[user, setUser] = useState({});
-  const[isLoggedIn, setIsLoggedIn] = useState(false);
+  const login = async () => {
+    try {
+      const currentUser = await firebaseApp.signInWithGoogle();
 
-  const login = async() => {
-    const currentUser = await firebaseApp.signInWithGoogle();
-    if(!currentUser){
-      console.log("Sign-in failed")
-    }
-    else {
+      if (!currentUser) {
+        console.log("Sign-in failed");
+        return;
+      }
+
       setUser(currentUser);
-      setIsLoggedIn(true)
-      redirect('/')
+      setIsLoggedIn(true);
+      redirect("/");
+
+    } catch (error) {
+        console.error("Login Failed", error);
+        return;
     }
-  }
- 
+  };
+
   return (
-    <AuthProvider value={{user, isLoggedIn, login}}>
-      <Home/>
+    <AuthProvider value={{ user, isLoggedIn, login }}>
+      <Home />
     </AuthProvider>
-  )
+  );
 }
