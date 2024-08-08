43bd8257402b234d32caa08297035e8fdc11c8a1
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-07 19:02:35 -0600

    refactor: Update Home component image size and alignment
    The Home component in src/pages/Home/Home.jsx has been refactored to update the image size and alignment. The image now has a height of 3/4 and a width of 3/4, and it is centered within its container using flexbox. This change improves the visual presentation of the component and ensures consistent image display across different screen sizes.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -124,9 +124,9 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="h-full w-full">
+          <div className="h-full w-full flex justify-center items-center">
             <img
-              className="mx-auto h-full w-full rounded-md object-cover"
+              className="mx-auto h-3/4 w-3/4 rounded-md object-cover"
               src="https://images.unsplash.com/photo-1630673245362-f69d2b93880e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
               alt=""
             />
